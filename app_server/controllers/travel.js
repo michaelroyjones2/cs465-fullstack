ba5381fa935bbdba2914bddfd9a5874431c16c33
Michael Jones II <crosseyedCOBRA@gmail.com>
2022-02-10 18:46:30 -0500

    module 5 part 1 and 2 complete

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,52 @@
 var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 
-'utf8'));
+const { request } = require('http');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
 
-/* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', trips });
+const renderTravelList = (req, res, responseBody) => {
+    let message = null; 
+    let pageTitle = process.env.npm_package_description + ' -Travel';
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length){
+            message = 'No trips exist in database!';
+        }
+    }
+
+    res.render('travel',{
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
+};
+
+
+
+/* GET travelList view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
    };
-   module.exports = {
+
+   console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+   request(
+       requestOptions,
+       (err, { statusCode }, body) => {
+           if (err) {
+               console.error(err);
+           }   
+           renderTravelList(req, res, body);
+       }
+   )
+}
+module.exports = {
     travel
-   };
+};
