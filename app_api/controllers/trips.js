cf59d488db934c91aa77945e3880ecf70444bb4b
Michael Jones II <crosseyedCOBRA@gmail.com>
2022-02-19 18:31:38 -0500

    module 6 complete

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,3 +1,5 @@
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const async = require('hbs/lib/async');
 const mongoose = require('mongoose');
 const model = mongoose.model('trips');
@@ -34,9 +36,75 @@ const tripsFindCode = async (req, res) => {
     });
     };
 
+const tripsAddTrip = async (req, res) =. {
+    model
+    .create({
+        code: req.body.code,
+        name: req.body.code,
+        length: req.body.length,
+        start: req.body.start,
+        resort:req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body,description
+    },
+    (err, trip) => {
+        if (err) {
+            return res
+            .status(400)
+            .json(err);
+        }else {
+            return res
+            .status(201)
+            .json(trip);
+        }
+    });
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    trip
+
+    .findOneAndUpdate({ 'code': req.params.tripCode }, {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+        }, { new: true })
+        .then(trip => {
+        if (!trip) {
+        return res
+        .status(404)
+       .send({
+        message: "Trip not found with code " 
+       + req.params.tripCode
+        });
+        }
+        res.send(trip);
+        }).catch(err => {
+        if (err.kind === 'ObjectId') {
+        return res
+        .status(404)
+        .send({
+        message: "Trip not found with code " 
+       + req.params.tripCode
+        });
+        }
+        return res
+        .status(500) // server error
+        .json(err);
+        });
+       }
+
 module.exports = {
     tripsList,
     tripsFindCode
+    tripsAddTrip
+    tripsUpdateTrip
     
 
 
