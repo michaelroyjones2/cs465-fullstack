cf0a2350f273b38ff0bbd9b86d305feb4c1f67db
Michael Jones II <crosseyedCOBRA@gmail.com>
2022-02-26 17:26:19 -0500

    module 7 backend complete

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const hbs = require("hbs"); //This variable needs to be declared in order for the partials to load
+const passport = require('passport');
+
+require('./app_api/database/db');
+
+require('./app_api/config/passport');
+
 var indexRouter = require("./app_api/database/db");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
 const apiRouter = require('./app_api/routes/index');
+const req = require('express/lib/request');
 
 var app = express();
 
@@ -24,6 +33,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+
+app.use('./api',(req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+
+
+
+
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -3,6 +3,8 @@ const res = require('express/lib/response');
 const async = require('hbs/lib/async');
 const mongoose = require('mongoose');
 const model = mongoose.model('trips');
+const user = mongoose.model('users');
+
 
 const tripsList = async (req, res) =>{
     model   
@@ -36,33 +38,41 @@ const tripsFindCode = async (req, res) => {
     });
     };
 
-const tripsAddTrip = async (req, res) =. {
-    model
-    .create({
-        code: req.body.code,
-        name: req.body.code,
-        length: req.body.length,
-        start: req.body.start,
-        resort:req.body.resort,
-        perPerson: req.body.perPerson,
-        image: req.body.image,
-        description: req.body,description
-    },
-    (err, trip) => {
-        if (err) {
-            return res
-            .status(400)
-            .json(err);
-        }else {
-            return res
-            .status(201)
-            .json(trip);
-        }
-    });
-}
+const tripsAddTrip = async (req, res) => {
+        console.log('tripsAddTrip invoked with: \n' + req.body);
+        getUser(req, res,
+            (req, res) => {
+        trip
+            .create({
+                code: req.body.code,
+                name: req.body.code,
+                length: req.body.length,
+                start: req.body.start,
+                resort:req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body,description
+            },
+                (err, trip) => {
+                    if (err) {
+                        return res
+                            .status(400)
+                            .json(err);
+                    }else {
+                        return res
+                            .status(201)
+                            json(trip);
+                    }
+                });
+            }
+        )
+    }
 
+        
 const tripsUpdateTrip = async (req, res) => {
     console.log(req.body);
+    getUser(req, res,
+        (req, res) => {
     trip
 
     .findOneAndUpdate({ 'code': req.params.tripCode }, {
@@ -98,12 +108,39 @@ const tripsUpdateTrip = async (req, res) => {
         .status(500) // server error
         .json(err);
         });
+    }
+    )
        }
+
+
+const getUser = (req, res, callback) => {
+    if (req.payload && req.payload.email) {
+        user    
+            .findOne({ email: req.payload.email })
+            .exec((err, user) => { 
+                if (!user) {
+                    return res  
+                        .status(404)
+                        .json({"message": "User not found"});
+                } else if (err) {
+                    console.log(err);
+                    return res
+                        .status(404)
+                        .json(err);
+                }
+                callback(req, res, user.name);
+            });
+    } else {
+        return res  
+            .status(404)
+            .json({"message": "User not found"});
+    }
+};
 
 module.exports = {
     tripsList,
-    tripsFindCode
-    tripsAddTrip
+    tripsFindCode,
+    tripsAddTrip,
     tripsUpdateTrip
     
 
