dc76a189ca5b07c4b2f124d13d695395c7adb303
Michael Jones II <crosseyedCOBRA@gmail.com>
2022-02-26 19:39:06 -0500

    module 7 front and back end complete

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,14 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
-
+import { AuthResponse } from '../models/authresponse';
+import { BROWSER_STORAGE } from '../storage';
 import { Trip } from '../models/trip';
+import { User } from '../models/user';
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripDataService {
 
-  constructor(private http: Http) { }
+  constructor(
+    private http: Http,
+    @Inject(BROWSER_STORAGE) private storage: Storage,
+    private tripDataService: TripDataService 
+      
+    
+    ) { }
 
   private apiBaseUrl = 'http://localhost:3000/api/';
   private tripUrl = `${this.apiBaseUrl}trips/`;
@@ -45,6 +57,33 @@ export class TripDataService {
     console.error('Something has gone wrong', error); //for demo purposes only
     return Promise.reject(error.message || error);
   }
+  public getToken(): string {
+    return this.storage.getItem('travlr-token');
+   }
+   public saveToken(token: string): void {
+    this.storage.setItem('travlr-token', token);
+   }
+
+  public login(user: User): Promise<any> {
+    return this.tripDataService.login(user)
+    .then((authResp: AuthResponse) => 
+   this.saveToken(authResp.token));
+   }
+
+   public register(user: User): Promise<any> {
+    return this.tripDataService.register(user)
+    .then((authResp: AuthResponse) => 
+   this.saveToken(authResp.token));
+   }
+
+   private makeAuthApiCall(urlPath: string, user: User): Promise<AuthResponse>{
+     const url: string = `${this.apiBaseUrl}/${urlPath}`;
+     return this.http
+      .post(url, user)
+      .toPromise()
+      .then(response => response.json() as AuthResponse)
+      .catch(this.handleError);
+   }
 
 
 }
--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -3,6 +3,7 @@ import { Router } from "@angular/router";
 // import { trips } from '../data/trips';
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication';
 
 
 
@@ -26,6 +27,7 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
+    private authService: AuthenticationService,
     private router: Router
     ) { }
 
@@ -44,6 +46,11 @@ export class TripListingComponent implements OnInit {
     this.trips = foundTrips;
     });
 }
+
+public isLoggedIn(): boolean {
+  return this.authService.isLoggedIn();
+}
+
     ngOnInit(): void {
       this.getTrips();
 }
